4ba5e6f7c5cd6b6bc3d38793e8f23b4639bd35a5
c <>
2021-12-13 21:47:13 +0000

    wip

--- game/scenes/Patterns.gd
+++ game/scenes/Patterns.gd
@@ -1,15 +1,27 @@
 extends Node2D
 
 
+var pattern_dir = "res://patterns"
 var Pattern = preload("res://scenes/Pattern.tscn")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    # TODO iterate the folder
-    var p = Pattern.instance()
-    p.file = "res://patterns/red.json"
-    add_child(p)
+	var d = Directory.new()
+	d.open(pattern_dir)
+	d.list_dir_begin()
+	var f = d.get_next()
+	var i = 0
+	while f != "":
+		if f != "." and f != ".." and not d.current_is_dir():
+			print_debug(f)
+			i += 1
+			var p = Pattern.instance()
+			p.file = d.get_current_dir() + "/" + f
+			add_child(p)
+		f = d.get_next()
+
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- game/scenes/PatternsControl.gd
+++ game/scenes/PatternsControl.gd
@@ -1,11 +1,5 @@
 extends Control
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,11 +7,15 @@ func _ready():
 
 func add_pattern(pattern):
 	var l = Label.new()
-	l.text = pattern.pattern_name[0]
+	if not pattern.unlocked:
+		l.text = "???"
+	else:
+		l.text = pattern.pattern_name[0]
 	pattern.connect("unlocked", self, "_on_Pattern_unlocked", [l, pattern])
 	# TODO connect the unlocked signal and active the button or something
 	$VBoxContainer.add_child(l)
 
 func _on_Pattern_unlocked(control, pattern):
 	print_debug("unlocked")
+	control.text = pattern.pattern_name[0]
 	control.modulate = Color(0, 1, 0)
