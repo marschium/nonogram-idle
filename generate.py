54873146f96217db358d3e6c65a56d9f9561d080
c <>
2022-01-29 20:52:51 +0000

    wip disable bonus iamge gen

--- generate.py
+++ generate.py
@@ -12,8 +12,11 @@ if __name__ == "__main__":
         tools.pattern.gen(os.path.join("images", f), os.path.join("game", "patterns", dest))
         shutil.copy2(os.path.join("images", f), os.path.join("game", "images", f))
 
-    with open("bonuses.csv") as f:
-        for line in f:
-            bonus = line.split(",")
-            name = bonus[0]
-            tools.combine.halfhalf(os.path.join("images", bonus[1].strip() + ".png"), os.path.join("images", bonus[2].strip() + ".png"), f"game/images/bonuses/{name}.png")
+    # with open("bonuses.csv") as f:
+    #     for line in f:
+    #         bonus = line.split(",")
+    #         name = bonus[0]
+    #         tools.combine.halfhalf(
+    #             os.path.join("images", bonus[1].strip() + ".png"),
+    #             os.path.join("images", bonus[2].strip() + ".png"),
+    #             f"game/images/bonuses/{name}.png")
