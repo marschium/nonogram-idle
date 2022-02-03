394a6654e8a77300012286e00b222291d4f098f2
Your Name <you@example.com>
2022-02-03 22:43:24 +0000

    made bonus label fix container

--- game/scenes/ui/BonusLabel.gd
+++ game/scenes/ui/BonusLabel.gd
@@ -3,28 +3,30 @@ extends Control
 var bonus = null
 
 func _set_text():
-    $Panel/Label.text = "%s\n%ds left" % [bonus.text, bonus.remaining()]
+    $Control/Panel/Label.text = "%s\n%ds left" % [bonus.text, bonus.remaining()]
 
 func _ready():
-    bonus.connect("deactivated", self, "_on_Bonus_deactivated")
-    $ProgressBar.value = 0
-    $ProgressBar.max_value = bonus.time
-    $PanelContainer/TextureRect.texture = load("res://images/bonuses/%s.png" % [bonus.bonus_name.to_lower()])
+    if bonus != null:
+        bonus.connect("deactivated", self, "_on_Bonus_deactivated")
+        $VBoxContainer/ProgressBar.value = 0
+        $VBoxContainer/ProgressBar.max_value = bonus.time
+        $VBoxContainer/TextureRect.texture = load("res://images/bonuses/%s.png" % [bonus.bonus_name.to_lower()])
     
 
 func _process(delta):
     if bonus != null:
-        $ProgressBar.value = bonus.elapsed()
+        $VBoxContainer/ProgressBar.value = bonus.elapsed()
         self._set_text()
-
-func _on_PanelContainer_mouse_entered():
-    if not $Panel.visible:
-        self._set_text()
-        $Panel.visible = true
-
-
-func _on_PanelContainer_mouse_exited():
-    $Panel.visible = false
 
 func _on_Bonus_deactivated():
     queue_free()
+
+
+func _on_BonusLabel_mouse_entered():
+    if not $Control/Panel.visible:
+        self._set_text()
+        $Control/Panel.visible = true
+
+
+func _on_BonusLabel_mouse_exited():
+    $Control/Panel.visible = false
