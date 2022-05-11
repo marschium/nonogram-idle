d422518ab5089fa9380316d2893f80c10e97d1cc
Your Name <you@example.com>
2022-05-11 23:55:14 +0100

    fullscreen checkbox correctly displayed

--- game/scenes/ui/MenuToolbar.gd
+++ game/scenes/ui/MenuToolbar.gd
@@ -20,6 +20,8 @@ func _ready():
 func _on_ViewMenuButtonPopup_id_pressed(id):
     if id == VIEW_FULLSCREEN:
         OS.window_fullscreen = !OS.window_fullscreen
+        var idx = $PanelContainer/MarginContainer/HBoxContainer/ViewMenuButton.get_popup().get_item_index(id)
+        $PanelContainer/MarginContainer/HBoxContainer/ViewMenuButton.get_popup().set_item_checked(idx, OS.window_fullscreen)
     elif id == VIEW_STATS:
         var s = StatsWindow.instance()
         get_parent().add_child(s)
